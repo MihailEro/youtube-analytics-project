f104fcd48e1679c0a5fe27d15de3f301fb870cbe
MihailEro <mihailerohin235@gmail.com>
2023-06-25 19:00:39 +0300

    add try and except

--- src/video.py
+++ src/video.py
@@ -8,14 +8,19 @@ class Video:
     youtube = build('youtube', 'v3', developerKey=api_key)
 
     def __init__(self, video_id: str) -> None:
-        self.__video_id = video_id
         video_response = Video.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
                                                      id=video_id
                                                      ).execute()
-        self.video_title = video_response['items'][0]['snippet']['title']
-        self.video_url = 'https://www.youtube.com/watch?v=' + self.__video_id
-        self.view_count = int(video_response['items'][0]['statistics']['viewCount'])
-        self.like_count = int(video_response['items'][0]['statistics']['likeCount'])
+        try:
+            self.video_title = video_response['items'][0]['snippet']['title']
+            self.video_url = 'https://www.youtube.com/watch?v=' + self.__video_id
+            self.view_count = int(video_response['items'][0]['statistics']['viewCount'])
+            self.like_count = int(video_response['items'][0]['statistics']['likeCount'])
+        except:
+            self.title = None
+            self.video_url = None
+            self.view_count = None
+            self.like_count = None
 
     def __str__(self):
         return self.video_title
